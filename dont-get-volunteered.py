4e3819db7ca6aae39f4fd0931748ff91a72e41e1
Jake Mullins <jakemullins0@outlook.com>
2023-01-12 20:12:32 -0700

    Paradigm shift 2

--- dont-get-volunteered.py
+++ dont-get-volunteered.py
@@ -1,21 +1,43 @@
-final = 0
+def solution(src, dest):
+    return solution2(src // 8, src % 8, dest // 8, dest % 8, 0)
 
-# #def move(src_x, src_y, dest_x, dest_y, count):
-#     # print((src_x, src_y), (dest_x, dest_y), count)
-# 
-#     if count > 20:
-#         return
-# 
-#     # base case
-#     if src_x == dest_x and src_y == dest_y:
-#         final = count
-#         print("YALLAH ", final)
-# 
-#     offsets = [(-2, -1), (-2, 1), (-1, -2), (-1, 2), (1, -2), (1, 2), (2, 1), (2, -1)]
-# 
-#     for offset in offsets:
-#         # if within range
-#         if src_x + offset[0] > 0 and src_x + offset[0] < 63 and src_y + offset[1] > 0 and src_y + offset[1] < 63:
-#             move(src_x + offset[0], src_y + offset[1], dest_x, dest_y, count + 1)
-#             
-print(solution(27, 21))
+def solution2(src_x, src_y, dest_x, dest_y, count):
+    offsets = [(2, 1), (2, -1), (-2, 1), (-2, -1), (1, 2), (1, -2), (-1, 2), (-1, -2)]
+
+    Q = list()
+
+    explored = dict()
+    explored[(src_x, src_y)] = 0
+
+    count = 1
+
+    v = (src_x, src_y)
+    Q.append(v)
+
+    while len(Q) != 0:
+        u = Q.pop(0)
+        print(u)
+
+        if u[0] == dest_x and u[1] == dest_y:
+            print("final:", Q, count)
+            return count
+        
+        for offset in offsets:
+            potential_coord = (src_x + offset[0], src_y + offset[1])
+            if potential_coord[0] > 0 and potential_coord[0] < 63 and potential_coord[1] > 0 and potential_coord[1] < 63:
+                if potential_coord not in explored:
+                    v = potential_coord
+                    Q.append(v)
+                    explored[potential_coord] = count
+
+        count += 1
+# class Node:
+#     def __init__(self, x, y, dist=0):
+#         self.x = x
+#         self.y = y
+#         self.dist = dist
+#         explored = False
+
+offset = [(2, 1), (2, -1), (-2, 1), (-2, -1), (1, 2), (1, -2), (-1, 2), (-1, -2)]
+
+solution(28, 13)
