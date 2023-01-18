32dc362bd8adc485ca62485e0845683672b32759
Jake Mullins <jakemullins0@outlook.com>
2023-01-18 12:16:04 -0700

    Finished staircase

--- the-grandest-staircase-of-them-all.py
+++ the-grandest-staircase-of-them-all.py
@@ -1,104 +1,35 @@
-def solution(n):        # https://web.archive.org/web/20090220004749/http:/www.site.uottawa.ca/~ivan/F49-int-part.pdf
-    solutions = []
-    solutions.append([n])
-    x = [1] * n 
-    solutions.append(x[0:len(x)])   # weird pass by reference behavior
-    x[0] = n
-    m = 1   # number of parts
-    h = 1   # index of last part > 1
-    while x[0] != 1:
-        print(1, x)
-        if x[h] == 2:
-            m = m + 1
-            x[h] = 0                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    
-            h = h - 1
-        else:
-            r = x[h] - 1
-            t = m - h + 1
-            x[h] = r
-            while t >= r:
-                h = h + 1
-                print(x, h)
-                x[h] = r
-                t = t - r
-            if t == 0:
-                m = h
-            else:
-                m = h + 1
-                if t > 1:
-                    h = h + 1
-                    x[h] = t
-            solutions.append(x[0:len(x)])
-        print(2, x)
-    return solutions
+# Here are some dead ends because I've never heard the term "Dynamic programming".
+# https://web.archive.org/web/20090220004749/http:/www.site.uottawa.ca/~ivan/F49-int-part.pdf
+# https://jeromekelleher.net/generating-integer-partitions.html
 
-# pretty close, look at the x[-1] and x[0], wtf is that notation? # https://web.archive.org/web/20090220004749/http:/www.site.uottawa.ca/~ivan/F49-int-part.pdf
-def solution2(n):
-    solutions = []
-    x = [1] * n
-    solutions.append(x[0:n])
-    x[-1] = -1
-    x[0] = 2
-    h = 1
-    m = n - 1
-    solutions.append(x[0:m])
+# probably could do caching, haven't learned that yet + sys import is illegal
+def solution(n):
+    global lookup 
+    lookup = [[-1 for j in range(n + 1)] for i in range(n + 2)]
 
-    ctr = 0
+    return evaluate_stair(1, n) - 1
 
-    while x[0] != n:
-        if (m - h) > 1:
-            h = h + 1
-            x[h] = 2
-            m = m - 1
-        else:
-            j = m - 2
-            while x[j] == x[m - 1]:
-                x[j] = 1
-                j -= 1
-            h = j + 1
-            x[h] = x[m - 1] + 1
-            r = x[m] + x[m - 1] * (m - h - 1)
-            x[m] = 1
-            if m - h > 1:
-                x[m - 1] = 1
-            m = h + r - 1
-            
-        solutions.append(x[0:m])
-        ctr += 1
-    return solutions
+def evaluate_stair(height, left):
+    global lookup
 
-def all_unique_partitions(n):
-    solutions = []
+    if lookup[height][left] != -1:
+        return lookup[height][left]
 
-    p = [0] * n
-    k = 0
-    p[k] = n
+    # All bricks will be consumed
+    # save literally a single recursive call at the end of a search tree by evaluating if it would return true before the right hand search begins.
+    # I tried to benchmark this using time command, but it ran so fast that any changes were up to CPU Scheduling. Good problems to have.
+    if left == 0:
+        return 1
 
-    solutions.append(p[0:n])
+    # Not enough bricks to complete a row, thus an invalid result.
+    if left < height:
+        return 0
 
-    while True:
+    # Left case is increasing the height of that step.
+    # Right case is "placing" that stair, subtracting that height from total amount, moving on to the next step.
+    lookup_res = evaluate_stair(height + 1, left) + evaluate_stair(height + 1, left - height)
+    
+    # save data
+    lookup[height][left] = lookup_res
 
-        # find rightmost value != 1
-        rem_val = 0
-        while k >= 0 and p[k] == 1:
-            rem_val += p[k]
-            k -= 1
-
-        if k < 0:
-            return solutions
-
-        p[k] -= 1
-        rem_val += 1
-
-        while p[k] < rem_val:
-            p[k + 1] = p[k]
-            rem_val -= p[k]
-            k += 1
-
-        p[k + 1] = rem_val
-        k += 1
-
-        solutions.append(p[0:k + 1])
-        print(p[0:k + 1])
-
-print(solution3(5))
+    return lookup_res
